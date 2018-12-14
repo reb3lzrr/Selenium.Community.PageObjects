9ae677d8e093fa2e4643e4b560cda0c9acc68cc8
Koen Klever <boarderkoen@hotmail.com>
2018-12-14 16:15:52 +0100

    Removed unsed references

--- SeleniumExtras.PageObjects.Tests/Proxies/WebElementEnumerableProxyTests.cs
+++ SeleniumExtras.PageObjects.Tests/Proxies/WebElementEnumerableProxyTests.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using FluentAssertions;
 using Moq;
 using OpenQA.Selenium;
 using SeleniumExtras.PageObjects.Proxies;
